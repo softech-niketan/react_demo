08d5bb822705187763d6346dcec18ce5ad71d7e3
softech-niketan <niketan.p@softechsolutionsindia.in>
2024-03-14 22:00:23 +0530

    first react code commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import Nav from "./components/nav.js"
+import Header from "./components/header.js"
+// import Home from "./components/home.js"
+import Home from "./components/home.js"
+
+import Footer from "./components/footer.js"
+import "./App.css";
+import React from "react";
+ 
+// import Footer from   "./src/Components/Footer";
+function App() {
+  return (
+   
+ <>
+ <Nav />
+ <Header />
+ <Home />
+
+ <Footer />
+ </>
+
+
+   
+);
+}
+
+export default App;
---
+++ src/components/header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+// import logo from './images/logo.png'; 
+export default function header() {
+  return (
+    <header class="masthead" style={{ backgroundImage: 'url("./assets/img/home-bg.jpg")' }}>
+    <div class="container position-relative px-4 px-lg-5">
+        <div class="row gx-4 gx-lg-5 justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-7">
+                <div class="site-heading">
+                    <h1>Clean Blog</h1>
+                    <span class="subheading">A Blog Theme by Start Bootstrap</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+    
+  );
+}
+
